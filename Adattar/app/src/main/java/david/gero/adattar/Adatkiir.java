b4d5f3e8d0152752ad876bd44e3e1c7e24e5c148
Geragamer <gerodavid@gmail.com>
2015-12-16 23:46:51 +0100

    Egy új java osztály létrehozása történt meg Itt az adatok egyenlőre még csak meghatározásra kerültek. pl: lesz Név adat ide kérve     lesz Email adat ide kérve stb

---
+++ Adattar/app/src/main/java/david/gero/adattar/Adatkiir.java
@@ -0,0 +1,18 @@
+package david.gero.adattar;
+
+/**
+ * Created by Dávid on 2015.12.16..
+ */
+public class Adatkiir {
+
+    private String _Nev, _Email, _Telefonszam, _Lakcim;
+
+    public Adatkiir (String Nev, String Email, String Telefonszam, String Lakcim){
+
+        _Nev = Nev;
+        _Email = Email;
+        _Telefonszam = Telefonszam;
+        _Lakcim = Lakcim;
+
+    }
+}
