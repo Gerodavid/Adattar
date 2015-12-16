9b865e235115c2b622796f5f5a433754662bfdb2
Geragamer <gerodavid@gmail.com>
2015-12-17 00:04:43 +0100

    Itt már meghatároztuk, hogy hova fog kiíródni az adat a mainactivity-ben, de még adatot nem továbbít.

--- Adattar/app/src/main/java/david/gero/adattar/MainActivity.java
+++ Adattar/app/src/main/java/david/gero/adattar/MainActivity.java
@@ -81,6 +81,9 @@ public class MainActivity extends AppCompatActivity {
     }
     private class ContactListAdapter extends ArrayAdapter<Adatkiir>{
 
+        public ContactListAdapter(){
+            super(MainActivity.this,R.layout.adatkiir_lista, Adatkiiras);
+        }
     }
 }
 
